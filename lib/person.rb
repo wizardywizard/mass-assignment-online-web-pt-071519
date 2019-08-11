4ac3a9ff0805a9e9505fccd5bc98fae33f2e746b
David Little <littledavid175@gmail.com>
2019-08-11 22:01:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,7 @@
 class Person
   
+  attr_accessor 
+  
   def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width)
