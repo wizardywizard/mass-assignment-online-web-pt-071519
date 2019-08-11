bb0f98182b30866d0e4c5499b86e2eb24f0ce3a7
David Little <littledavid175@gmail.com>
2019-08-11 22:03:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
-    
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
   
 end
